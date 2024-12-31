9edde627c1a27632d4888542a87034991d758538
Coffee☕️ <coffee@coffeebot.dev>
2024-12-31 11:23:45 -0500

    feat: update custom bridge tokens

--- data/customBridgeTokens.ts
+++ data/customBridgeTokens.ts
@@ -18,29 +18,39 @@ export type CustomBridgeToken = {
 export const customBridgeTokens: CustomBridgeToken[] = [
   {
     chainId: 1,
-    l1Address: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
-    l2Address: "0x703b52F2b28fEbcB60E1372858AF5b18849FE867",
+    l1Address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    l2Address: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
     bridges: [
       {
-        label: "txSync Bridge",
-        iconUrl: "/img/txsync.png",
-        depositUrl: "https://portal.txsync.io/bridge/?token=0x703b52F2b28fEbcB60E1372858AF5b18849FE867",
-        withdrawUrl: "https://portal.txsync.io/bridge/withdraw/?token=0x703b52F2b28fEbcB60E1372858AF5b18849FE867",
+        label: "Stargate",
+        iconUrl: "/img/stargate.svg",
+        depositUrl:
+          "https://stargate.finance/bridge?srcChain=ethereum&srcToken=0xdAC17F958D2ee523a2206206994597C13D831ec7&dstChain=abstract&dstToken=0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
+        withdrawUrl:
+          "https://stargate.finance/bridge?srcChain=abstract&srcToken=0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4&dstChain=ethereum&dstToken=0xdAC17F958D2ee523a2206206994597C13D831ec7",
       },
     ],
-    symbol: "wstETH",
-    bridgedSymbol: "wstETH",
-    name: "Wrapped liquid staked Ether 2.0",
-    bridgingDisabled: true,
+    symbol: "USDT",
+    bridgedSymbol: "USDT",
+    name: "Tether",
   },
   {
     chainId: 1,
     l1Address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    l2Address: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
-    learnMoreUrl: "https://www.circle.com/blog/native-usdc-now-available-on-zksync",
-    bridges: [],
+    l2Address: "0x703b52F2b28fEbcB60E1372858AF5b18849FE867",
+    bridges: [
+      {
+        label: "Stargate",
+        iconUrl: "/img/stargate.svg",
+        depositUrl:
+          "https://stargate.finance/bridge?srcChain=ethereum&srcToken=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48&dstChain=abstract&dstToken=0x84A71ccD554Cc1b02749b35d22F684CC8ec987e1",
+        withdrawUrl:
+          "https://stargate.finance/bridge?srcChain=abstract&srcToken=0x84A71ccD554Cc1b02749b35d22F684CC8ec987e1&dstChain=ethereum&dstToken=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      },
+    ],
     symbol: "USDC",
     bridgedSymbol: "USDC.e",
     name: "USD Coin",
+    bridgingDisabled: true,
   },
 ];
